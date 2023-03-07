ca4aa26b76127e586d70b11c677d66f4fa87ae75
Michael Thomas <michaelthomas0721@icloud.com>
2023-03-07 17:17:23 -0500

    Feat: Finished Cart page

--- components/CartItem.vue
+++ components/CartItem.vue
@@ -1,4 +1,8 @@
 <script>
+import { OhVueIcon, addIcons } from "oh-vue-icons";
+import { HiTrash } from "oh-vue-icons/icons";
+
+addIcons(HiTrash);
 export default {
   props: {
     item: {
@@ -6,19 +10,62 @@ export default {
       default: 0,
     },
   },
+  components: {
+    "v-icon": OhVueIcon,
+  },
+  methods: {
+    HandleQuantity(e) {
+      if (!localStorage) return;
+      let cart = JSON.parse(localStorage.getItem("items"));
+      cart.forEach((item) => {
+        if (item.id === this.item.id && item.size === this.item.size) {
+          item.quantity = e.target.value;
+        }
+      });
+      localStorage.setItem("items", JSON.stringify(cart));
+      this.$emit("update")
+    },
+    HandleDelete() {
+      if (!localStorage) return;
+      let cart = JSON.parse(localStorage.getItem("items"));
+      cart.forEach((item, index) => {
+        if (item.id === this.item.id && item.size === this.item.size) {
+          cart.splice(index, 1);
+        }
+      });
+      localStorage.setItem("items", JSON.stringify(cart));
+      this.$emit("delete", this.item)
+    }
+  }
 };
 </script>
 
 <template>
-  <div class="w-full h-36 relative flex flex-row">
+  <div class="w-full h-36 relative flex flex-row shadow-lg">
     <img
       :src="'/assets/images' + item.imageFolder + '/1.png'"
       class="h-full aspect-square object-cover"
     />
     <div class="flex flex-col">
       <p>{{ item.name }}</p>
-      <p>{{ item.price }}</p>
-      <p>{{ item.size }}</p>
+      <p>${{ item.price }}</p>
+      <p>Size: {{ item.size }}</p>
+      <div class="flex flex-row gap-3">
+        <p>Quantity:</p>
+        <input type="number" :value="item.quantity" min="1" class="w-10 text-center" @change="HandleQuantity" />
+      </div>
+    </div>
+    <div class="ml-auto flex flex-row items-end p-4">
+      <button @click="HandleDelete" class="rounded-full">
+        <v-icon
+          name="hi-trash"
+          speed="slow"
+          animation="wrench"
+          hover="true"
+          scale="1.5"
+          fill="#0e1f2c"
+        />
+      </button>
     </div>
   </div>
 </template>
--- pages/cart.vue
+++ pages/cart.vue
@@ -13,34 +13,11 @@ export default {
     };
   },
   mounted() {
-    if (!localStorage) return;
-    let items = JSON.parse(localStorage.getItem("items"));
-    if (items) {
-      let shoeItems = [];
-      for (let item in items) {
-        let shoe = DataFetcher({
-          type: "shoes",
-          value: { type: "id", value: items[item].id, final: true },
-        });
-        if (shoe) shoe = shoe[0];
-        shoe.size = items[item].size;
-        shoe.quantity = items[item].quantity;
-        shoeItems.push(shoe);
-      }
-      this.cartItems = shoeItems;
-      this.cartItemsNum = shoeItems.length;
-      let sTotal = 0;
-      shoeItems.forEach((item) => {
-        sTotal += item.price;
-      });
-      this.subtotal = sTotal;
-      this.taxes = sTotal * 0.13;
-      this.total = this.subtotal + this.taxes;
-    }
+    this.GetCart();
   },
   methods: {
     Checkout() {
-      let data = fetch("http://localhost:3000/api/stripe", {
+      fetch("http://localhost:3000/api/stripe", {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
@@ -54,23 +31,72 @@ export default {
           window.location.href = data.url;
         });
     },
+    HandleDelete(item) {
+      console.log("YUUHHHH");
+      this.cartItems = this.cartItems.filter(
+        (cartItem) => cartItem.id !== item.id || cartItem.size !== item.size
+      );
+      this.UpdateCartState();
+      this.CalculateValues();
+    },
+    GetCart() {
+      if (!localStorage) return;
+      let items = JSON.parse(localStorage.getItem("items"));
+      if (items) {
+        let shoeItems = [];
+        for (let item in items) {
+          let shoe = DataFetcher({
+            type: "shoes",
+            value: { type: "id", value: items[item].id, final: true },
+          });
+          if (shoe) shoe = shoe[0];
+          shoe.size = items[item].size;
+          shoe.quantity = items[item].quantity;
+          shoeItems.push(shoe);
+        }
+        this.cartItems = shoeItems;
+        this.cartItemsNum = shoeItems.length;
+        this.CalculateValues();
+      }
+    },
+    UpdateCartValue() {
+      this.GetCart();
+      this.CalculateValues();
+    },
+    UpdateCartState() {
+      this.$emit("updateCart");
+    },
+    CalculateValues() {
+      let sTotal = 0;
+      this.cartItems.forEach((item) => {
+        sTotal += item.price * item.quantity;
+      });
+      this.subtotal = sTotal;
+      this.taxes = sTotal * 0.13;
+      this.total = this.subtotal + this.taxes;
+    },
   },
 };
 </script>
 
 <template>
   <div class="max-w-7xl w-full h-full mx-auto my-5">
-    <div class="flex flex-row justify-between">
-      <div class="flex flex-col gap-1 max-w-3xl w-full">
+    <div class="flex flex-row flex-wrap gap-6 mx-auto justify-center">
+      <div class="flex flex-col gap-5 max-w-md w-full">
         <p class="text-2xl mb-3">Cart</p>
-        <CartItem v-for="cartItem in cartItems" :item="cartItem" />
+        <CartItem
+          v-for="cartItem in cartItems"
+          :item="cartItem"
+          @delete="HandleDelete"
+          @update="UpdateCartValue"
+        />
       </div>
-      <div class="text-lg max-w-sm w-full flex flex-col gap-3">
+      <div class="text-lg max-w-md w-full flex flex-col gap-3">
         <p class="text-2xl mb-3">Checkout</p>
         <CheckoutText title="Subtotal" :value="'$' + subtotal" />
         <CheckoutText title="Estimated Shipping" :value="'Free'" />
         <CheckoutText title="Estimated Taxes" :value="'$' + taxes" />
-        <div class="w-full h-[1px] bg-gray-600 my-3"/>
+        <div class="w-full h-[1px] bg-gray-600 my-3" />
         <CheckoutText title="Total" :value="'$' + total" />
         <button
           @click="Checkout"
